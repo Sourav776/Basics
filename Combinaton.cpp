1e9c93e80988c38a60068563d763440955ba3e12
Sourav Mondal <souraviit776.bd@gmail.com>
2018-04-25 14:59:21 +0600

    Add files via upload

---
+++ Combinaton.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+map<char, int> mp;
+vector<char> v;
+set<string> st;
+set<string> ::iterator it;
+
+int main()
+{
+    char s[] = {'a', 'a', 'z', 'd', 'e', 'f', '\0'};
+
+    sort(s, s+6);
+    string a;
+
+    for(int i = 1; i < (1<<6); i++){
+        int cnt = 0;
+        for(int j = 0; j < 31; j++){
+            if(i & (1<<j))
+            {
+                if(!mp[s[j]]){
+                    cnt++;
+                    v.push_back(s[j]);
+                }
+                mp[s[j]]++;
+            }
+        }
+        mp.clear();
+        if(cnt == 3){
+            a="";
+            for(int j = 0; j < v.size(); j++){
+                a += v[j];
+            }
+            st.insert(a);
+            cnt=0;
+        }
+        v.clear();
+    }
+
+
+
+    for(it = st.begin(); it != st.end(); it++){
+        cout << *it << endl;
+    }
+
+
+    return 0;
+}
+/*
+5 Sourav 112
+9 sohel 122
+8 gobinda 15
+6 somu 12
+2 ahsin 125
+6 arnob 12
+*/
+
